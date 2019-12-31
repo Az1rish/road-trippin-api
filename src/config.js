5f7fcd16acc81674cef9fb60152852341adef43e
Shane <shanemcneil19@gmail.com>
2019-12-30 18:57:32 -0700

    complete config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: '',
-  }
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN: '',
+  DB_URL: process.env.DB_URL || 'postgresql://road-trippin:road-trippin@localhost/road-trippin',
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+}
