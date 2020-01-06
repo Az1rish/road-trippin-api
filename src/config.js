07a7640f775033c65c112a66d885f7e08eadd514
Shane <shanemcneil19@gmail.com>
2020-01-06 08:48:38 -0700

    change typo

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
   CLIENT_ORIGIN: 'http://localhost:3000',
   DB_URL: process.env.DB_URL || 'postgresql://road_trippin:road_trippin@localhost/road_trippin',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api'
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
 }
