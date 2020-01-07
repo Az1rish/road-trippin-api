96facbecb4913e9b24fe0ed1914db5dc6b726c01
Shane <shanemcneil19@gmail.com>
2020-01-07 07:55:31 -0700

    change DB_URL to DATABASE_URL for heroku deployment

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ORIGIN: 'http://localhost:3000',
-  DB_URL: process.env.DB_URL || 'postgresql://road_trippin:road_trippin@localhost/road_trippin',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://road_trippin:road_trippin@localhost/road_trippin',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://road_trippin:road_trippin@localhost/road_trippin_test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
 }
--- test/setup.js
+++ test/setup.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const { expect } = require('chai')
 const supertest = require('supertest')
 
-process.env.TEST_DB_URL = process.env.TEST_DB_URL
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
   || "postgresql://road_trippin:road_trippin@localhost/road_trippin_test"
 
 global.expect = expect
