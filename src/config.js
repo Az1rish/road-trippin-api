8fb85455199671d9a50a0efc3b92ab63ac08eb8c
Shane <shanemcneil19@gmail.com>
2020-01-07 18:58:35 -0700

    change origin to reflect zeit deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: 'http://localhost:3000',
+  CLIENT_ORIGIN: 'https://road-trippin.now.sh/',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://road_trippin:road_trippin@localhost/road_trippin',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://road_trippin:road_trippin@localhost/road_trippin_test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
