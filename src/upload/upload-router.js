3e3aa16a372da12f02558812581570719acc7613
Shane <shanemcneil19@gmail.com>
2020-01-11 11:10:41 -0700

    trying to connect db to s3

--- src/upload/upload-router.js
+++ src/upload/upload-router.js
@@ -2,19 +2,22 @@ const express = require("express");
 const uploadRouter = express.Router();
 const PhotoService = require('../photos/photos-service')
 const UploadService = require('./upload-service');
-
 const singleUpload = UploadService.single('image');
 
-uploadRouter.post('/image', function(req, res) {
 
-  singleUpload(req, res, function(err) {
 
-    if (err) {
-      return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
-    }
-    PhotoService.insertPhoto(res.json)
-    return res.json({'imageUrl': req.file.location});
-  });
-});
+uploadRouter
+    .post('/image', function(req, res) {
+        
+        singleUpload(req, res, function(err) {
+            if (err) {
+                return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
+            }
+            
+            PhotoService.insertPhoto(req.app.get('db'), req.file.location)
+            return res.json({'imageUrl': req.file.location});
+        });
+
+    });
 
 module.exports = uploadRouter;
