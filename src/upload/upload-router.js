35240b1414cc5819128424121c96f49d2c46f888
Shane <shanemcneil19@gmail.com>
2020-01-13 09:13:35 -0700

    image post is being added to db, YAY!

--- src/upload/upload-router.js
+++ src/upload/upload-router.js
@@ -10,7 +10,8 @@ uploadRouter
     .post('/image', function(req, res) {
         console.log("Starting uploadRouter.post /image")
         singleUpload(req, res, function(err) {
-            console.log("single upload successful" + ` req is: ${req}`)
+            console.log("single upload successful")
+            // console.log(req)
             if (err) {
                 return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
             }
@@ -18,8 +19,9 @@ uploadRouter
             const image = req.file.location
             const { title, content } = req.body
             const newPhoto = { title, image, content }
-            return PhotoService.insertPhoto(req.app.get('db'), newPhoto);
-            console.log("db insertion successful")
+            // console.log(newPhoto)
+            return PhotoService.insertPhoto(req.app.get('db'), newPhoto).then(res.json({ 'upload': "success"}))
+            // console.log("db insertion successful")
             // return res.json({'imageUrl': req.file.location});
         });
 
