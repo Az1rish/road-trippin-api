94c655cd0e86c7a5224e759afb3adb858c75771b
Shane <shanemcneil19@gmail.com>
2020-01-14 08:32:40 -0700

    change photos table, build upload capability

--- migrations/001.do.create_road_trippin_photos.sql
+++ migrations/001.do.create_road_trippin_photos.sql
@@ -2,6 +2,7 @@ CREATE TABLE road_trippin_photos (
   id SERIAL PRIMARY KEY,
   image TEXT,
   title TEXT NOT NULL,
+  location TEXT NOT NULL,
   content TEXT,
   date_created TIMESTAMP DEFAULT now() NOT NULL
 );
--- src/upload/upload-router.js
+++ src/upload/upload-router.js
@@ -2,29 +2,30 @@ const express = require("express");
 const uploadRouter = express.Router();
 const PhotoService = require('../photos/photos-service')
 const UploadService = require('./upload-service');
-const singleUpload = UploadService.single('image');
+const formUpload = UploadService.single('image');
 
 
 
 uploadRouter
     .post('/image', function(req, res) {
-        console.log("Starting uploadRouter.post /image")
-        singleUpload(req, res, function(err) {
-            console.log("single upload successful")
+        // console.log("Starting uploadRouter.post /image")
+        formUpload(req, res, function(err) {
+            // console.log("single upload successful")
             // console.log(req)
             if (err) {
                 return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
             }
-            console.log("about to insert image to db")
+            // console.log("about to insert image to db")
             const image = req.file.location
-            const { title, content } = req.body
-            const newPhoto = { title, image, content }
+            const { title, location, content } = req.body
+            console.log(req.body)
+            const newPhoto = { title, image, location, content }
             // console.log(newPhoto)
-            return PhotoService.insertPhoto(req.app.get('db'), newPhoto).then(res.json({ 'upload': "success"}))
+            return PhotoService.insertPhoto(req.app.get('db'), newPhoto)
+                .then(res.json({ newPhoto }))
             // console.log("db insertion successful")
             // return res.json({'imageUrl': req.file.location});
         });
-
     });
 
 module.exports = uploadRouter;
