ae9ea04518ce6e38f5669147ef117acdcc05bfd8
Shane <shanemcneil19@gmail.com>
2020-01-14 20:25:30 -0700

    work on upload

--- src/upload/upload-router.js
+++ src/upload/upload-router.js
@@ -13,16 +13,16 @@ uploadRouter
             // console.log("single upload successful")
             // console.log(req)
             if (err) {
-                return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
+                return res.status(422).send({ error: err.message });
             }
             // console.log("about to insert image to db")
-            const image = req.file.location
-            const { title, location, content } = req.body
+            // const image = req.file.location
+            // const { image, title, location, content } = req.body
             console.log(req.body)
-            const newPhoto = { title, image, location, content }
+            // const newPhoto = { title, image, location, content }
             // console.log(newPhoto)
-            return PhotoService.insertPhoto(req.app.get('db'), newPhoto)
-                .then(res.json({ newPhoto }))
+            // return PhotoService.insertPhoto(req.app.get('db'), newPhoto)
+                // .then(res.json({ newPhoto }))
             // console.log("db insertion successful")
             // return res.json({'imageUrl': req.file.location});
         });
