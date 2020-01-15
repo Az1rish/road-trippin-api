a7cff6c28957222b595958ac54b0d9f89098245b
Shane <shanemcneil19@gmail.com>
2020-01-15 06:02:13 -0700

    remove comments from upload router

--- src/upload/upload-router.js
+++ src/upload/upload-router.js
@@ -2,27 +2,29 @@ const express = require("express");
 const uploadRouter = express.Router();
 const PhotoService = require('../photos/photos-service')
 const UploadService = require('./upload-service');
-const formUpload = UploadService.single('image');
+const formUpload = UploadService.any();
 
 
 
 uploadRouter
     .post('/image', function(req, res) {
-        // console.log("Starting uploadRouter.post /image")
+        console.log("Starting uploadRouter.post /image")
         formUpload(req, res, function(err) {
-            // console.log("single upload successful")
+            console.log("form upload successful")
             // console.log(req)
             if (err) {
                 return res.status(422).send({ error: err.message });
             }
-            // console.log("about to insert image to db")
-            // const image = req.file.location
-            // const { image, title, location, content } = req.body
+            console.log(req.files)
             console.log(req.body)
-            // const newPhoto = { title, image, location, content }
-            // console.log(newPhoto)
-            // return PhotoService.insertPhoto(req.app.get('db'), newPhoto)
-                // .then(res.json({ newPhoto }))
+            console.log("about to insert image to db")
+            const image = req.files[0].location
+            const { title, location, content } = req.body
+            
+            const newPhoto = { title, image, location, content }
+            console.log(newPhoto)
+            return PhotoService.insertPhoto(req.app.get('db'), newPhoto)
+                .then(res.json({ newPhoto }))
             // console.log("db insertion successful")
             // return res.json({'imageUrl': req.file.location});
         });
